4ee6492bcd3e51b78449422cc1ae7755026fe219
KarolDawidG <karoldawidg@gmail.com>
2024-01-01 01:02:07 +0100

    iptables command added

--- run_monitor.sh
+++ run_monitor.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 output="Analyzer"
 source_files="main.cpp utils/utils.cpp protocol_analysis.cpp"
-log_file="analyzer.log"
+log_directory="logs"
+log_file_prefix="anomalyDetector"
+log_file="logs/mainLogFile.txt"
+threshold=1000
+
 find_latest_log_file() {
-    ls -Art anomalyDetector-*.txt 2>/dev/null | tail -n 1
+    ls -Art $log_directory/$log_file_prefix-*.txt 2>/dev/null | tail -n 1
 }
+
+block_suspicious_ips() {
+    echo "Blokowanie podejrzanych adresów IP..."
+
+    latest_log_file=$(find_latest_log_file)
+    if [[ -r "$latest_log_file" ]]; then
+        while read -r line; do
+            IP=$(echo "$line" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+            COUNT=$(echo "$line" | grep -oE "Ilość: [0-9]+" | grep -oE "[0-9]+")
+
+            if [[ $COUNT -gt $threshold ]]; then
+                sudo iptables -A INPUT -s $IP -j DROP
+                echo "Zablokowano IP: $IP"
+            fi
+        done < "$latest_log_file"
+    else
+        echo "Nie znaleziono lub nie można odczytać pliku logów: $latest_log_file"
+    fi
+}
+
+
+unblock_all_ips() {
+    echo "Odblokowywanie wszystkich zablokowanych adresów IP..."
+    sudo iptables -L INPUT -n --line-numbers | grep DROP | awk '{print $1}' | sort -r | while read line; do
+        sudo iptables -D INPUT $line
+    done
+    echo "Wszystkie adresy IP zostały odblokowane."
+}
+
 
 # Sprawdzenie, czy g++ i pcap są zainstalowane
 if ! command -v g++ &> /dev/null || ! ldconfig -p | grep -q libpcap; then
@@ -34,7 +67,10 @@ echo "Wybierz opcję uruchomienia programu:"
 echo "1) Uruchom w tle."
 echo "2) Uruchom w terminalu."
 echo "3) Wyswietl podejrzane adresy IP."
-echo "4) Zakoncz dzialanie programu."
+echo "4) Zablokuj podejrzane adresy IP"
+echo "5) Odblokuj podejrzane adresy IP"
+echo "6) Wyswietl iptable z lista blokowanych IP"
+echo "9) Zakoncz dzialanie programu."
 read -p "Wybór: " choice
 
 case $choice in
@@ -44,11 +80,12 @@ case $choice in
         clear
         echo "Program uruchomiony w tle, PID: $!"
         echo $! > program.pid   #plik z PID programu
+        echo
         exec $0 # Uruchomienie skryptu ponownie
         ;;
     2)
         # Uruchomienie programu w terminalu
-        ./$output | tee logs/mainLogFile.txt
+        ./$output | tee $log_file
         ;;
     3)
         # Czyta logi i wyswietla podejrzane IP
@@ -64,6 +101,26 @@ case $choice in
         exec $0
         ;;
     4)
+        clear
+        # Uruchamia funkcję do blokowania podejrzanych IP
+        block_suspicious_ips
+        echo
+        exec $0 
+        ;;
+    5)
+        clear
+        # Uruchamia funkcję do odblokowania podejrzanych IP
+        unblock_all_ips
+        echo
+        exec $0 
+        ;;
+    6)
+        clear
+        sudo iptables -L INPUT -n --line-numbers
+        echo
+        exec $0
+        ;;
+    9)
         # Wyjście ze skryptu
         killall Analyzer
         echo "Wyjście."
